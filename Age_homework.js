4e4714b83d901bae0dbb04b63d672ff278c6c012
uliana7774 <78162032+uliana7774@users.noreply.github.com>
2022-04-19 16:57:53 +0300

    Add files via upload

---
+++ Age_homework.js
@@ -0,0 +1,15 @@
+let age_1 = 10;
+let age_2 = 18;
+let age_3 = 60;
+if (age_1<age_2){
+console.log(" You don't have access cause your age is " + age_1 + " it's less than " + age_2)
+}
+else if (age_1>=age_2 && age_1<age_3){
+console.log("Welcome!")
+}
+else if (age_1>age_3){
+    console.log("Keep calm and look Culture channel")
+}
+else {
+    console.log("Technical work");
+}
