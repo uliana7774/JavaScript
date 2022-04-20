6ddc4f54fa8882476e8fb83ef796eaa471df46ff
uliana7774 <78162032+uliana7774@users.noreply.github.com>
2022-04-20 13:18:13 +0300

    Add files via upload

---
+++ item_homework.js
@@ -0,0 +1,41 @@
+let item_1;// Создать переменную “item_1”
+item_1 = 5;//Присвоить переменной item_1 цифру 5.
+
+console.log (item_1);//Вывести в консоль item_1.
+let item_2;//Создать переменную “item_2”
+item_2 = 3;//Присвоить переменной item_2 цифру 3.
+
+
+let item_3;//Создать переменную “item_3”
+
+item_3 = item_1 + item_2;//Присвоить переменной item_3 сложение item_1 и item_2.
+
+console.log(item_3);//Вывести в консоль item_3.
+let item_4;//Создать переменную “item_4”
+item_4 = "Yolochka";//Присвоить переменной item_4 строку “Yolochka”
+console.log (item_4);//Вывести в консоль item_4.
+
+let result = item_3 + item_4;
+console.log(result);//Вывести в консоль сложение item_3 и item_4.
+
+console.log(item_3 * item_4);
+console.log(result);//Вывести в консоль умножение item_3 и item_4.
+let item_5;// Создать переменную “item_5”
+item_5 = item_3;//Присвоить переменной item_5 переменную item_3
+
+let item_6;// Создать переменную item_6.
+let item_6_type;// Создать переменную item_6_type
+
+item_6 = 15;//Присвоить переменной item_6 значение 15
+
+console.log('item_6=='+ typeof(item_6));//Присвоить переменной item_6_type тип переменной item_6
+console.log('item_6_type=='+ typeof(item_6_type));//Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——  
+let item_7 = String(item_6) //Создать переменную item_7 и в ней преобразовать item_6 в String.
+let item_7_type = typeof(item_7) //Присвоить переменной item_7_type тип переменной item_7
+console.log('item_7 == ' + typeof(item_7)) //Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,
+console.log('item_7_type == ' + typeof(item_7_type)) //Вывести в консоль тип данных item_7 в виде —— “item_7_type == ”  item_7_type
+
+
+
+
+
